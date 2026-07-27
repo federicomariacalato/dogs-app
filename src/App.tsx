8cb75200276b456fb847ed06d555578808d011e3
Rosario Floridia <rosariofloridia35@gmail.com>
2026-07-27 19:54:44 +0200

    Aggiunte immagini in base alla razza

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,41 @@ import "./App.css";
 import Hero from "./homePage/Hero";
 import { DogCard } from "./homePage/DogCard";
 import { useState, useEffect } from "react";
-import getBreedList from "./data/dog.service";
+import getBreedList, { getBreedImage } from "./data/dog.service";
 
 
-type Breed = {
+export type Breed = {
     breed: string;
     subBreeds: any;
+    imgSrc?: string;
   }
 
 function App() {
   const [breeds, setBreeds] = useState<Breed[]>([])
-  const [error, setError] = useState()
 
   useEffect(() => {
-     getBreedList().then((value) => {
+     getBreedList().then(async (value) => {
         console.log(value)
+        for (let index = 0; index < value.length; index++) {
+          const element = value[index];
+          element.imgSrc = await getBreedImage(element.breed)
+        }
         setBreeds(value)
       })
   },[])
 
   return (
     <>
-      <Hero />
-      {breeds.map((breed) => (<DogCard
-        imgSrc="https://www.viridea.it/wp-content/uploads/2008/04/Golden-Retriever.jpg"
-        title={breed.breed}
-        paragraph={`Questa è la popolarissima razza ${breed.breed}.`}
-      />))}
+      <Hero>
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+          {breeds.map((breed) => (<DogCard
+            imgSrc={breed.imgSrc ?? "https://www.viridea.it/wp-content/uploads/2008/04/Golden-Retriever.jpg"}
+            title={breed.breed}
+            key={breed.breed}
+            paragraph={`Questa è la popolarissima razza ${breed.breed}.`}
+          />))}
+        </div>
+      </Hero>
     </>
   );
 }
--- src/data/dog.service.ts
+++ src/data/dog.service.ts
@@ -6,7 +6,9 @@
 //     .then(data => data.message)
 // }
 
-function getBreedList(){
+import type { Breed } from "../App"
+
+function getBreedList():Promise<Breed[]>{
     return fetch("https://dog.ceo/api/breeds/list/all")
     .then(response => response.json())
     .then(json => {
@@ -18,5 +20,12 @@ function getBreedList(){
         })
 })
 }
+
+export function getBreedImage(breed:string){
+    return fetch (`https://dog.ceo/api/breed/${breed}/images`)
+    .then(response => response.json())
+    .then(data => data.message[0])
+}
+
 
 export default getBreedList
